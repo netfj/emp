85918969723e8e94da13b6d8746e0b0ce9b86ee0
netfj <netfj@sina.com>
2019-04-02 00:05:01 +0800

    2019-3-31 III

--- ex/01.py
+++ ex/01.py
@@ -3,17 +3,12 @@
 # @Author:Netfj@sina.com @File:01.py @Time:2019/3/31 9:09
 
 
-a = {1:1,2:2,3:3}
+a=[]
+a.append((1,2,'3'))
+a.append((1,2,'3'))
+a.append((1,2,'3'))
+print(a)
 
-b = a.keys()
-print(b)
 
-w = [i for i in b]
-
-print(w)
-print(w[0])
-print(w[1])
-print(w[2])
-
-for i in range(2,2):
-    print(i)
+a,b ,c =0,0,9
+print(a,b,c)
--- word2table.py
+++ word2table.py
@@ -16,7 +16,7 @@ logset()
 
 class pickup_emp():
     filelist = ['emp_sample.docx']
-    db_table_0_a = {
+    db_table_0a = {
         'name':'姓名', 'gender':'性别', 'birthday':'出生年月',
         'nation':'民族', 'native':'籍贯','birthplace':'出生地',
         'party_time':'入党时间', 'work_time':'参加工作时间','health':'健康状况',
@@ -25,8 +25,7 @@ class pickup_emp():
         'education2':'在职教育', 'academy2':'毕业院校系及专业',
         'post_now':'现任职务',
         'post_will':'拟任职务',
-        'post_remove':'拟免职务',
-        'end':'简历'
+        'post_remove':'拟免职务'
     }
     db_table_0_b = {
         'resume_time':'简历时间','resume_post':'简历岗位'
@@ -40,7 +39,7 @@ class pickup_emp():
     # 内部数据初始化
     def initialize(self):
         self.docx_file = ''             # 导入的 WORD 文件名：sample.docx
-        self.tables_info = {}           # 提取的表的初始数据：原始信息
+        self.table_info = {}            # 提取的表的初始数据：原始信息
         self.table_info_clean = {}      # 清洗处理过后的数据：可用信息
         self.data2db = {}               # 将要导入到数据表中的数据：精准信息
         logging.debug('内部数据初始化')
@@ -57,41 +56,94 @@ class pickup_emp():
 
     # 更换文件
     def updata_word_file(self, docx_file):
-        print(docx_file)
-        self.initialize()  # 将内部数据初始化
-        self.docx_file = docx_file  # 更换文件名
-        doc = docx.Document(self.docx_file)  # 打开文件
-        self.tables = doc.tables  # 获取文件中的表格集
+        self.initialize()                       # 将内部数据初始化
+        self.docx_file = docx_file              # 更换文件名
+        doc = docx.Document(self.docx_file)     # 打开文件
+        self.tables = doc.tables                # 获取文件中的表格集 句柄
 
-        # 收集有关信息, 保存在：self.tables_info['tables_info']
-        tb_info = {'文件名':self.docx_file}                   # 文件名
-        tb_info.update({'表格数量':str(len(self.tables))})  # 表格数量
+        # 收集有关信息, 保存在：self.table_info['table_info']
+        tb_info = {'文件名':self.docx_file}                     # 文件名
+        tb_info.update({'表格数量':str(len(self.tables))})      # 表格数量
         for n in range(0,len(self.tables)):
             row = len(self.tables[n].rows)
             col = len(self.tables[n].columns)
             tb_info.update({'表格{}的行列数量（行，列）'.format(n):(row,col)})
-        self.tables_info.update(tables_info = tb_info)
+        self.table_info.update(table_info = tb_info)
 
-    # 提取表的数据，保存在：self.tables_info['items_text']
+    # 提取表的数据，以 列表 形式，保存在-->字典：self.table_info['items_text']
+    ''' 数据结构：列表，每项的含义{表的序号: [(行号，列号，内容TEXT), ...] 
+    实例：{0: [(0, 0, '姓\u3000名'), (0, 1, '姓\u3000名'), 
+            (0, 2, '张某某'), (0, 3, '性\u3000别'), (0, 4, '性\u3000别'), ...]
+    '''
     def extract_from_word(self):
+        d = {}
         for n in range(0, len(self.tables)):
+            x = []
             row = len(self.tables[n].rows)
             col = len(self.tables[n].columns)
-            self.table_items[n] = []
             for r in range(0, row):
                 for c in range(0, col):
-                    t = self.get_cell_text(table=n, row=r, col=c)
-                    self.table_items[n].append(t)
-        logging.info(self.table_items)
+                    t = self.tables[n].cell(r, c).text
+                    x.append((r,c,t))
+            d.update({n:x})
+        self.table_info.update({'items_text':d})
+
+    # 清洗数据  ： 1.有效取值，2.去空格、头尾换行符
+    # 保存至字典： self.table_info_clean
+    def extract_from_word_clean(self):
+        # 处理表1的上半部分（简历之前）
+        data = []
+
+        data += self.get_line_0a(key='姓名')
+        data += self.get_line_0a(key='民族')
+        data += self.get_line_0a(key='入党时间')
+        data += self.get_line_0a(key='专业技术职务')
+        data += self.get_line_0a(key='全日制教育')
+        data += self.get_line_0a(key='在职教育')
+        data += self.get_line_0a(key='现任职务')
+        data += self.get_line_0a(key='拟任职务')
+        data += self.get_line_0a(key='拟免职务')
+
+        print(data)
+
+        xm = self.db_table_0a
+        print(xm)
+
+
+
+    def get_line_0a(self, key='项目名称'):
+        ds = self.table_info['items_text'][0]  # 提取的原始数据
+        for cell in ds:
+            if set(cell[2]) & set(key) == set(key):
+                row = cell[0]
+                break
+        lt = []
+        for cell in ds:
+            if cell[0] == row:
+                lt.append(cell[2].replace(' ', '').replace('　', '').replace('\n',''))
+
+        #去重
+        lt2 = []
+        for x in lt:
+            if len(lt2) == 0:       # 新列表为空时，先放一个元素
+                lt2.append(x)
+                continue
+            if x != lt2[-1]:        # 新追加时，查看与新列表最后一个元素是否相等
+                lt2.append(x)
+
+        return lt2
 
     def run(self):
-        logging.info('文件列表:'+  ('|').join(self.filelist))
+        logging.info('导入文件列表:'+  ('|').join(self.filelist))
         for f in self.filelist:
-            self.updata_word_file(f)
-            logging.debug(self.tables_info)
+            self.updata_word_file(f)        # 更换当前导入文件
+            self.extract_from_word()        # 提取原始数据
+            logging.debug(self.table_info)  # 写入日志
+            self.extract_from_word_clean()  # 清洗数据
+
+
 
 filelist = ['emp_sample.docx','emp_xxb.docx']
-filelist = [ filelist[0] ]
 w = pickup_emp(filelist)
 
 import sys
